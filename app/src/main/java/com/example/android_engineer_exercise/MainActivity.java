bceec67fd00a34009ea8c46447d1dddabd982fda
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2022-04-02 21:09:08 +0100

    Added code to MainActivity2

--- app/src/main/java/com/example/android_engineer_exercise/MainActivity.java
+++ app/src/main/java/com/example/android_engineer_exercise/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.android_engineer_exercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button changeActivityBTN = findViewById(R.id.create_an_account);
+        changeActivityBTN
+        .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeActivity();
+            }
+
+        });
+    }
+
+    private void changeActivity() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
