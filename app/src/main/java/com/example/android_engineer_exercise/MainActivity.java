2a540d0b7ce17fab525f869a3af243e18be015ef
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2022-04-02 23:26:31 +0100

    Added to the App UI

--- app/src/main/java/com/example/android_engineer_exercise/MainActivity.java
+++ app/src/main/java/com/example/android_engineer_exercise/MainActivity.java
@@ -10,14 +10,12 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private Button button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button changeActivityBTN = findViewById(R.id.create_an_account);
+        Button changeActivityBTN = findViewById(R.id.btn_create_account);
         changeActivityBTN
         .setOnClickListener(new View.OnClickListener() {
             @Override
