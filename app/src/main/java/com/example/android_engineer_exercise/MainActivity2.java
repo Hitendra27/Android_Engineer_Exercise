4690e91f1c103a2b740ddac4db76db9489382f91
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2022-04-04 13:16:29 +0100

    Working on Validation

--- app/src/main/java/com/example/android_engineer_exercise/MainActivity2.java
+++ app/src/main/java/com/example/android_engineer_exercise/MainActivity2.java
@@ -6,8 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    EditText enter_email, enter_password, repeat_password;
+    Button btn_search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
